2f85af9ed5d0ef760e99d3073852d8f253590b6f
Brandon Derrill <Derrillbrandon@gmail.com>
2021-10-04 20:36:00 -0500

    More debugging for deployment

--- components/Views/Men.js
+++ components/Views/Men.js
@@ -1,10 +1,10 @@
 import Ouroboros from "../../IMG/Ouroboros.jpg";
-import Mike_Black_shirt from "../../IMG/Mike_Black_shirt.jpg";
+import Mike_Black_Shirt from "../../IMG/Mike_Black_Shirt.jpg";
 import ClassicLogoLS from "../../IMG/ClassicLogoLS.jpg";
 
 export default () => `
 <img src=" ${Ouroboros} ">
-<img src=" ${Mike_Black_shirt} ">
+<img src=" ${Mike_Black_Shirt}">
 <img src=" ${ClassicLogoLS} ">
 
 
