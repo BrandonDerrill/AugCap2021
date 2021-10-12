3a5d2769b3f5a9f765ccdb15960ee13d8a70ea9b
Brandon Derrill <Derrillbrandon@gmail.com>
2021-10-12 11:13:15 -0500

    Working pages

--- components/Views/About.js
+++ components/Views/About.js
@@ -9,6 +9,6 @@ export default () => `
 <h1>We Create the Apparel. You Re-Create the Paradigm.</h1>
     <p>
 
-        Good Karma was developed in 2017 under the influence of a reviving culture, by positioning it in the forefront of luxury street wear brands.  In a world where culture may be stripped, covered and buried, it is essential to know your worth and stand out from the stereotypical shadows that convey a misled image.  Good Karma is lifestyle brand that represents  being the best version of  self, and is conveyed through the brand owner's mantra, “Being Supreme”.  This mantra emanates individualism, mental quality and self-perception. Good karma promotes trendy styles, aspirational living and positive propaganda. We believe when one trusts in self, deception will never overrule.
+        Good Karma was developed in 2017 under the influence of a reviving culture, by positioning it in the forefront of luxury street wear brands. In a world where culture may be stripped, covered and buried, it is essential to know your worth and stand out from the stereotypical shadows that convey a misled image. Good Karma is lifestyle brand that represents being the best version of self, and is conveyed through the brand owner's mantra, “Being Supreme”.  This mantra emanates individualism, mental quality and self-perception. Good karma promotes trendy styles, aspirational living and positive propaganda. We believe when one trusts in self, deception will never overrule.
     </p>
 </div>`;
